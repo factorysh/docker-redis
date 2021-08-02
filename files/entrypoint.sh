09c8275eaf753375373b34b38012e15795e2bb08
Wilfried OLLIVIER <wollivier@bearstech.com>
2021-08-02 16:46:34 +0200

    Add on demand snapshots

--- files/entrypoint.sh
+++ files/entrypoint.sh
@@ -5,4 +5,13 @@ if [ ! -e /etc/redis/redis.conf ]; then
     echo "Conf built."
 fi
 
+SNAPSHOT_VOLUME_DIR="/var/lib/redis/volume"
+
+if [ -d "$SNAPSHOT_VOLUME_DIR" ]; then
+    echo "Snapshots volume found, snapshots enabled."
+else
+    echo "Snapshots disabled (reason : no snapshot volume found)."
+    sed -i -e 's/^\(save \d+ \d+\)/# \1/' -e 's/^\(dir .*\)/# \1/' /etc/redis/redis.conf
+fi
+
 exec "$@"
